8cb717efa0edd3685b497a05bb5b56c6faf8f6bb
Akihiro Urushihara <a.urusihara@gmail.com>
2026-05-15 21:40:36 +0900

    Delegate stream close to underlying Nostr streams for proper cleanup

--- nostr/src/commonMain/kotlin/work/socialhub/planetlink/nostr/action/NostrStream.kt
+++ nostr/src/commonMain/kotlin/work/socialhub/planetlink/nostr/action/NostrStream.kt
@@ -1,5 +1,9 @@
 package work.socialhub.planetlink.nostr.action
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import work.socialhub.knostr.social.stream.NotificationStream
 import work.socialhub.knostr.social.stream.TimelineStream
 import work.socialhub.planetlink.model.Stream
@@ -14,11 +18,17 @@ class NostrStream(
     override val isOpened: Boolean
         get() = _isOpened
 
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
     override suspend fun open() {
         _isOpened = true
     }
 
     override fun close() {
+        scope.launch {
+            timelineStream?.stop()
+            notificationStream?.stop()
+        }
         _isOpened = false
     }
 }
