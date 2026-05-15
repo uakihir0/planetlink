d8bd1fbb3d5fcb840950345b3153780bb900862c
Akihiro Urushihara <a.urusihara@gmail.com>
2026-05-15 21:39:41 +0900

    Remove duplicate isNostr extension from nostr module

--- nostr/src/commonMain/kotlin/work/socialhub/planetlink/nostr/expand/ServiceEx.kt
+++
@@ -1,9 +0,0 @@
-package work.socialhub.planetlink.nostr.expand
-
-import work.socialhub.planetlink.model.Service
-
-object ServiceEx {
-
-    val Service.isNostr: Boolean
-        get() = "nostr" == type.lowercase()
-}
