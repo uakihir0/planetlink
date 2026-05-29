1b8b2af4e6e85bf9096468cf4dc4817cc5c0f765
Akihiro Urushihara <a.urusihara@gmail.com>
2026-05-29 22:21:58 +0900

    test: add Nostr paging test
    Verify userCommentTimeLine paging works correctly
    and page1/page2 do not overlap.

--- all/src/jvmTest/kotlin/work/socialhub/planetlink/action/NostrRelayTest.kt
+++ all/src/jvmTest/kotlin/work/socialhub/planetlink/action/NostrRelayTest.kt
@@ -35,4 +35,31 @@ class NostrRelayTest : AbstractTest() {
             assertTrue(result.entities.isNotEmpty(), "userCommentTimeLine should return posts for an active user")
         }
     }
+
+    @Test
+    fun testNostrUserTimelinePaging() = runBlocking {
+        withTimeout(60_000) {
+            val account = nostr()
+            // fiatjaf - well-known active Nostr user (guaranteed to have many posts)
+            val id = Identify(account.service, ID("3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d"))
+
+            val page1 = account.action.userCommentTimeLine(id, Paging(5))
+            println("Nostr paging page1: ${page1.entities.size} posts")
+            page1.entities.forEach { println("  ${it.createAt} ${it.id}") }
+            assertTrue(page1.entities.isNotEmpty(), "page1 should return posts")
+
+            val nextPaging = page1.paging!!.pastPage(page1.entities)
+            val np = nextPaging as work.socialhub.planetlink.nostr.model.NostrPaging
+            println("Nostr nextPaging: since=${np.since} until=${np.until} count=${np.count}")
+
+            val page2 = account.action.userCommentTimeLine(id, nextPaging)
+            println("Nostr paging page2: ${page2.entities.size} posts")
+            page2.entities.forEach { println("  ${it.createAt} ${it.id}") }
+            assertTrue(page2.entities.isNotEmpty(), "page2 should return posts")
+
+            val page1Ids = page1.entities.map { it.id }
+            val page2Ids = page2.entities.map { it.id }
+            assertTrue(page1Ids.intersect(page2Ids.toSet()).isEmpty(), "page1 and page2 should not overlap")
+        }
+    }
 }
