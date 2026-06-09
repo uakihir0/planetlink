d54297a670ebc05eba1442768a2b35c77cebf517
Akihiro Urushihara <a.urusihara@gmail.com>
2026-06-09 20:27:56 +0900

    feat: add Capabilities class and capabilities() method to AccountAction

---
+++ core/src/commonMain/kotlin/work/socialhub/planetlink/action/Capabilities.kt
@@ -0,0 +1,41 @@
+package work.socialhub.planetlink.action
+
+import work.socialhub.planetlink.define.action.ActionType
+import kotlin.js.JsExport
+
+/**
+ * Capabilities
+ * アダプターがサポートする機能の一覧
+ */
+@JsExport
+open class Capabilities(
+    val supportedActions: Set<ActionType>
+) {
+
+    fun isSupported(action: ActionType): Boolean {
+        return action in supportedActions
+    }
+
+    @JsExport.Ignore
+    fun isAllSupported(vararg actions: ActionType): Boolean {
+        return actions.all { isSupported(it) }
+    }
+
+    @JsExport.Ignore
+    fun isAnySupported(vararg actions: ActionType): Boolean {
+        return actions.any { isSupported(it) }
+    }
+
+    fun isAllSupported(actions: Array<ActionType>): Boolean {
+        return actions.all { isSupported(it) }
+    }
+
+    fun isAnySupported(actions: Array<ActionType>): Boolean {
+        return actions.any { isSupported(it) }
+    }
+
+    @JsExport.Ignore
+    fun <T : ActionType> supportedOf(actions: Array<T>): List<T> {
+        return actions.filter { isSupported(it) }
+    }
+}
