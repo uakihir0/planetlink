bb56e5a9c354f1e38b4f7e9aaa71bb485491294a
Akihiro Urushihara <a.urusihara@gmail.com>
2026-07-16 15:22:38 +0900

    test(discord): cover paging end detection

---
+++ discord/src/commonTest/kotlin/work/socialhub/planetlink/discord/model/DiscordPagingTest.kt
@@ -0,0 +1,39 @@
+package work.socialhub.planetlink.discord.model
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class DiscordPagingTest {
+
+    @Test
+    fun marksPastEndForShortPage() {
+        val paging = DiscordPaging().apply {
+            count = 200
+        }
+
+        paging.setMarkPagingEnd(List(42) { Unit })
+
+        assertFalse(paging.isHasPast)
+    }
+
+    @Test
+    fun keepsPastForFullPage() {
+        val paging = DiscordPaging().apply {
+            count = 200
+        }
+
+        paging.setMarkPagingEnd(List(200) { Unit })
+
+        assertTrue(paging.isHasPast)
+    }
+
+    @Test
+    fun keepsPastWhenPageSizeIsUnknown() {
+        val paging = DiscordPaging()
+
+        paging.setMarkPagingEnd(emptyList<Unit>())
+
+        assertTrue(paging.isHasPast)
+    }
+}
